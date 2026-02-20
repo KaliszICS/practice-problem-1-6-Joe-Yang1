2d73b16437f674927866fd1a14fe474ec89be0a7
Joe-Yang1 <348993072@tdsb.ca>
2026-02-20 16:35:47 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/**
+		* File: Lesson 1.6 - Booleans
+		* Author: Joe Yang
+		* Date Created: February 13, 2023
+		* Date Last Modified: February 20, 2026
+		*/
+	
+	import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -11,26 +19,56 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
-		
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a boolean: ");
+	boolean bool1;
+	bool1 = input.nextBoolean();
+	System.out.println(bool1);
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	Scanner input = new Scanner(System.in)
+;	System.out.print("Input an integer: ");
+	int num;
+	num = input.nextInt();
+	System.out.println(num > 5);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input pizza: ");
+	String food;
+	food = input.nextLine();
+	System.out.println(food.equals( "pizza"));
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.println(num == 0 );
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input an integer: ");
+	int num;
+	num = input.nextInt();
+	System.out.println(num != 0);
 	}
 
 	public static void q6() {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a word earlier than google: ");
+	String word;
+	word = input.nextLine();
+	System.out.println(word.compareTo("google")<0);
 
 	}
 
